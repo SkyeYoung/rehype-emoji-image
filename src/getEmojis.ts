0e5da7e6ffe465b85ba1f12c78821f3efd7b8723
SkyeYoung <iskyex@outlook.com>
2023-04-19 09:16:09 +0800

    refactor: reduce size of getEmojis

--- src/getEmojis.ts
+++ src/getEmojis.ts
@@ -1,5 +1,5 @@
 import type { Gemoji } from 'gemoji';
-import { gemoji, nameToEmoji, emojiToName } from 'gemoji';
+import { gemoji } from 'gemoji';
 import emojiRegex from 'emoji-regex';
 
 const shortCodeRegExp = /:(\+1|[-\w]+):/g;
@@ -19,15 +19,15 @@ const getEmojis = (str: string): EmojiInfo[] => {
   const res: EmojiInfo[] = [];
 
   for (const match of str.matchAll(shortCodeRegExp)) {
-    const emoji = nameToEmoji[match[1]];
-    const ge = gemoji.find((v) => v.emoji === emoji);
+    const shortCode = match[1];
+    const ge = gemoji.find((v) => v.names.includes(shortCode));
     if (!ge) continue;
 
     const rawCode = match[0];
     const from = match.index || 0;
 
     res.push({
-      emoji,
+      emoji: ge.emoji,
       shortCode: match[1],
       gemoji: ge,
       from,
@@ -39,10 +39,11 @@ const getEmojis = (str: string): EmojiInfo[] => {
   for (const match of str.matchAll(emojiRegExp)) {
     const emoji = match[0];
     const from = match.index || 0;
+    const ge = gemoji.find((v) => v.emoji === emoji);
     res.push({
       emoji,
-      shortCode: emojiToName[emoji],
-      gemoji: gemoji.find((v) => v.emoji === emoji),
+      shortCode: ge.names[0],
+      gemoji: ge,
       from,
       to: from + emoji.length,
       raw: emoji,
